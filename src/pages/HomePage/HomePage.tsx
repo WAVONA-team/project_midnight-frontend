82d42c36530d5c7fd88fc007937892d5619dfaae
Jegor volkov <jegor.volkov.a@gmail.com>
2024-02-17 01:28:48 +0100

    feat(TAS-233): registration, login and confirmation pages

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomePage: React.FC = React.memo(() => {
+  return <p>Home</p>;
+});
+
+export default HomePage;
---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { LoginForm } from '@/modules/Authorization';
+
+const LoginPage: React.FC = React.memo(() => {
+  return <LoginForm />;
+});
+
+export default LoginPage;
