e4219e6b69ccac07fdbe4c8ca8b69e40bb21f13e
Jegor volkov <jegor.volkov.a@gmail.com>
2024-02-25 10:45:53 +0100

    feat(TAS-233): prepairing for confirmation reset password form

---
+++ src/modules/Authorization/components/ConfirmationResetPasswordForm/ConfirmationResetPasswordForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ConfirmationResetPasswordForm: React.FC = React.memo(() => {
+  return <form action="#"></form>;
+});
+
+export default ConfirmationResetPasswordForm;
