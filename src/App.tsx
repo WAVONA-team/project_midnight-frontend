5a05c8922eb30b022af14c2f90f6e5a08aa2f399
aguilam <pexfex@mail.ru>
2024-02-26 22:10:18 +0400

    feat(tas-239 and tas-238): added Music Player

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-
+import React, { ChangeEvent, useState } from 'react';
+import Player from './components/player/Player';
 import './App.css';
 
 const App: React.FC = React.memo(() => {
-  return 'App';
-});
+  const [url, setUrl] = useState('');
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUrl(event.target.value);
+  };
+  return (
+    <>
+      <Player url={url}/>
+      <input
+        type="text"
+        className='text-red-500'
+        value={url}
+        onChange={handleInputChange}
+      />
+    </>
+  );
+}
+)
 
 export default App;
