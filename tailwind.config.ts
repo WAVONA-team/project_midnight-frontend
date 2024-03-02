be21dfdf2cc07f9ccf175c3a7557297a339496cb
betterhell <dev.betterhell@gmail.com>
2024-03-02 07:28:07 +0300

    fix(TAS-270): change color names on kebab-case

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,27 +7,27 @@ export default {
         'hight': '#0B0D0B',
       },
       'error': {
-        'turkey_red': '#A90000',
+        'turkey-red': '#A90000',
       },
       'primary': {
         'poppy': '#DA2F37',
-        'fire_brick': '#BF222A',
+        'fire-brick': '#BF222A',
         'madder': '#9C1C22',
       },
-      'on_primary': {
-        'lavender_blush': '#FCEEEE',
-        'anti-flash_white': '#FFFFFF',
+      'on-primary': {
+        'lavender-blush': '#FCEEEE',
+        'anti-flash-white': '#FFFFFF',
       },
       'surface': {
         'eerie_black': '#171916',
         'black': '#0B0D0B',
       },
       'secondary': {
-        'cadet_gray': '#A09F9F',
-        'eerie_black_light': '#1D1F1C',
+        'cadet-gray': '#A09F9F',
+        'eerie-black-light': '#1D1F1C',
       },
-      'on_secondary': {
-        'dim_gray': '#696969',
+      'on-secondary': {
+        'dim-gray': '#696969',
         'platinum': '#E1E1E1'
       },
     },
