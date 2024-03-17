520eeeae79374e4c463d2d51249e6353caf0da49
Jegor volkov <jegor.volkov.a@gmail.com>
2024-03-17 22:30:52 +0100

    feat(TAS-299): loading page

--- src/pages/LoadingPage/LoadingPage.tsx
+++ src/pages/LoadingPage/LoadingPage.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
 
-export const LoadingPage: React.FC = React.memo(() => {
+import { Spinner } from '@/ui/Spinner';
 
+export const LoadingPage: React.FC = React.memo(() => {
   return (
-    <div>
-      <h1>PreloaderPage</h1>
+    <div
+      className="
+        h-screen
+        w-screen
+        bg-background-hight
+        relative
+        after:content-['']
+        after:block
+        after:absolute
+        after:w-full
+        after:h-full
+        after:bg-gradient-to-br
+        after:from-primary-fire-brick
+        after:opacity-20
+        after:z-10
+      "
+    >
+      <Spinner
+        width="w-12"
+        height="h-12"
+        thicknessWidth="w-9"
+        thicknessHeight="h-9"
+      />
+
+      <p className="block w-full text-center absolute z-20 top-1/2 left-1/2 -translate-x-1/2 translate-y-12 font-rubik font-semibold text-on-primary-lavender-blush text-base">
+        Подождите, идет загрузка
+      </p>
     </div>
   );
 });
