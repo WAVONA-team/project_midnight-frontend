d8a24b42a7120b5fdb7c55a1e09b3bd7b1f45d6d
Jegor volkov <jegor.volkov.a@gmail.com>
2024-03-17 15:32:20 +0100

    feat(TAS-233): reset password form markup

--- src/modules/Authorization/components/ResetPasswordForm/ResetPasswordForm.tsx
+++ src/modules/Authorization/components/ResetPasswordForm/ResetPasswordForm.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Controller, type SubmitHandler, useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { useStore } from '@/store';
 
 import { ServerErrors } from '@/shared/types/ServerErrors';
 
 import { ResetPasswordInputs } from '@/modules/Authorization/types';
+import { FormContainer } from '@/modules/Authorization/ui/FormContainer';
 
+import { MainButton, TextButtonLink } from '@/ui/Button';
 import { DefaultInput } from '@/ui/Input';
 
 const ResetPasswordForm: React.FC = React.memo(() => {
+  const [isButtonLoading, setIsButtonLoading] = useState(false);
   const navigate = useNavigate();
   const { reset } = useStore(({ reset }) => ({
     reset,
@@ -29,6 +32,8 @@ const ResetPasswordForm: React.FC = React.memo(() => {
   const onSubmit: SubmitHandler<ResetPasswordInputs> = async (formData) => {
     const { email } = formData;
 
+    setIsButtonLoading(true);
+
     await reset(email)
       .then(() => navigate('/reset-verify', { replace: true }))
       .catch(({ fieldErrors, formErrors }: ServerErrors) => {
@@ -46,26 +51,67 @@ const ResetPasswordForm: React.FC = React.memo(() => {
             message: formErrors,
           });
         }
-      });
+      })
+      .finally(() => setIsButtonLoading(false));
   };
 
   return (
-    <form action="#" onSubmit={handleSubmit(onSubmit)}>
-      {errors.root?.formErrors && <p>{errors.root.formErrors.message}</p>}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormContainer className="relative">
+        <Link
+          to="/"
+          className="text-on-primary-anti-flash-white block hover:text-on-primary-anti-flash-white text-sm"
+        >
+          Отменить
+        </Link>
 
-      <Controller
-        name="email"
-        control={control}
-        render={({ field }) => (
-          <DefaultInput
-            value={field.value}
-            onChange={(event) => field.onChange(event.target.value)}
-            error={errors.root?.email?.message}
+        <h2 className="text-on-primary-anti-flash-white font-rubik font-semibold text-2xl block mt-10 lg:font-openSans lg:font-normal">
+          Сброс пароля
+        </h2>
+
+        <div className="flex items-center mt-16">
+          <p className="text-on-primary-anti-flash-white text-sm flex items-center">
+            Еще нет аккаунта?
+          </p>
+
+          <TextButtonLink
+            title="Зарегистрироваться"
+            path="/register"
+            className="w-min"
           />
-        )}
-      />
+        </div>
 
-      <button>Submit</button>
+        {errors.root?.formErrors && (
+          <p className="text-error-imperial-red text-xs absolute top-40">
+            {errors.root.formErrors.message}
+          </p>
+        )}
+
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => (
+            <DefaultInput
+              className="mt-10"
+              labelText="Введите email"
+              placeholder="Email"
+              value={field.value}
+              onChange={(event) => field.onChange(event.target.value)}
+              error={errors.root?.email?.message}
+            />
+          )}
+        />
+
+        <div className="mt-16 grid grid-cols-3">
+          <MainButton
+            type="submit"
+            title="Продолжить"
+            handler={() => {}}
+            isLoading={isButtonLoading}
+            className="col-start-1 col-end-4 lg:col-start-3"
+          />
+        </div>
+      </FormContainer>
     </form>
   );
 });
--- src/pages/ResetPasswordPage/ResetPasswordPage.tsx
+++ src/pages/ResetPasswordPage/ResetPasswordPage.tsx
@@ -2,6 +2,28 @@ import React from 'react';
 
 import { ResetPasswordForm } from '@/modules/Authorization';
 
+import { Container } from '@/ui/Container';
+import { Logo } from '@/ui/Logo';
+
 export const ResetPasswordPage: React.FC = React.memo(() => {
-  return <ResetPasswordForm />;
+  return (
+    <div
+      className="
+    app
+    h-screen
+    bg-background-hight
+    lg:bg-[url('/src/assets/home_bg_desktop.webp')]
+    lg:bg-no-repeat
+    lg:bg-center
+    lg:bg-cover
+  "
+    >
+      <Container className="h-full flex flex-col justify-center">
+        <main className="lg:grid lg:grid-cols-2">
+          <Logo className="hidden lg:flex" />
+          <ResetPasswordForm />
+        </main>
+      </Container>
+    </div>
+  );
 });
