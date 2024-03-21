3eb24087e2cd70511c3fca603fa2e8c9ecb4cc25
Jegor volkov <jegor.volkov.a@gmail.com>
2024-03-21 21:57:26 +0100

    fix(TAS-239): fix directory name

---
+++ src/modules/Player/componentss/index.tsx
@@ -0,0 +1,6 @@
+export { LoopButton } from '@/modules/Player/componentss/LoopButton/LoopButton';
+export { Controls } from '@/modules/Player/componentss/Controls/Controls';
+export { TrackControlButtons } from '@/modules/Player/componentss/TrackControlButtons/TrackControlButtons';
+export { TimeRange } from '@/modules/Player/componentss/TimeRange/TimeRange';
+export { TrackInfo } from '@/modules/Player/componentss/TrackInfo/TrackInfo';
+export { VolumeChanger } from '@/modules/Player/componentss/VolumeChanger/VolumeChanger';
