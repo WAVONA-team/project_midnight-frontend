a530a72de45faf3a2a3d264238022470ffbedb85
betterhell <dev.betterhell@gmail.com>
2024-03-25 12:33:05 +0300

    feat(TAS-317): Complete SettingsPage.tsx & ConnectedAppsSettings.tsx

--- src/pages/ConnectedAppsSettings/ConnectedAppsSettings.tsx
+++ src/pages/ConnectedAppsSettings/ConnectedAppsSettings.tsx
@@ -1,10 +1,120 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export const ConnectedAppsSettings: React.FC = React.memo(() => {
+import { useStore } from '@/store';
+import { NormalizedUser } from 'project_midnight';
 
-  return (
-    <div>
-      <h1>ConnectedAppsSettings</h1>
-    </div>
-  );
-});
+import { ServiceCard } from '@/components/ServiceCard';
+
+import arrowIcon from '@/assets/arrows/arrowIcon.svg';
+import spotifyIcon from '@/assets/services/spotifyIcon.svg';
+import yandexMusicIcon from '@/assets/services/yandexMusicIcon.svg';
+
+export const ConnectedAppsSettings: React.FC<NormalizedUser> = React.memo(
+  ({ spotifyOAUTH, yandexOAUTH }) => {
+    const navigate = useNavigate();
+
+    const { registerSpotify } = useStore(({ registerSpotify }) => ({
+      registerSpotify,
+    }));
+
+    return (
+      <div
+        className="
+          font-rubik
+          bg-background-hight
+          text-on-primary-anti-flash-white
+          max-w-full
+          h-dvh
+          px-[16px]
+          py-[24px]
+          sm:bg-background-default-gradient
+          sm:max-w-full
+          sm:px-[80px]
+        "
+      >
+        <div className="flex">
+          <button
+            onClick={() => navigate(-1)}
+            className="
+              px-1.5
+              mr-2
+              focus:outline-none
+            "
+          >
+            <img src={arrowIcon} alt="arrow" />
+          </button>
+          <h1
+            className="
+              font-normal
+              text-base
+              tracking-wider
+              sm:text-2xl
+              sm:leading-10
+              sm:font-openSans
+              md:sm:text-3xl
+            "
+          >
+            Подключенные приложения
+          </h1>
+        </div>
+
+        <div
+          className="
+          flex
+          flex-wrap
+          gap-5
+          my-5
+          sm:mt-10
+        "
+        >
+          <ServiceCard
+            title="Spotify"
+            serviceIcon={
+              <div
+                className="
+                  rounded-md
+                  flex justify-center
+                  w-[48px] h-[48px]
+                  shadow-service-icon-spotify
+                  bg-service-icon-gradient
+                "
+              >
+                <img
+                  className="rounded-md"
+                  src={spotifyIcon}
+                  alt="Spotify Icon"
+                />
+              </div>
+            }
+            isConnected={spotifyOAUTH !== null}
+            handler={registerSpotify}
+          />
+
+          <ServiceCard
+            title="Yandex Music"
+            serviceIcon={
+              <div
+                className="
+                  rounded-md
+                  flex justify-center
+                  w-[48px] h-[48px]
+                  shadow-service-icon-yandex
+                  bg-service-icon-yandex-gradient
+                "
+              >
+                <img
+                  className="rounded-md"
+                  src={yandexMusicIcon}
+                  alt="Yandex Music Icon"
+                />
+              </div>
+            }
+            isConnected={yandexOAUTH !== null}
+            handler={() => {}}
+          />
+        </div>
+      </div>
+    );
+  },
+);
--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,10 +1,113 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { useStore } from '@/store';
+
+import arrowIcon from '@/assets/arrows/arrowIcon.svg';
 
 export const SettingsPage: React.FC = React.memo(() => {
+  const navigate = useNavigate();
+
+  const { logout } = useStore(({ logout }) => ({ logout }));
 
   return (
-    <div>
-      <h1>SettingsPage</h1>
+    <div
+      className="
+        font-rubik
+        bg-background-hight
+        text-on-primary-anti-flash-white
+        max-w-full
+        h-dvh
+        px-[16px]
+        py-[24px]
+        sm:bg-background-default-gradient
+        sm:max-w-full
+        sm:px-[80px]
+      "
+    >
+      <div className="flex">
+        <button
+          onClick={() => navigate(-1)}
+          className="
+            px-1.5
+            mr-2
+            focus:outline-none
+            sm:hidden
+          "
+        >
+          <img src={arrowIcon} alt="Arrow Icon" />
+        </button>
+        <h1
+          className="
+            font-normal
+            text-base
+            tracking-wider
+            sm:text-4xl
+            sm:leading-10
+            sm:mb-3
+            sm:font-openSans
+          "
+        >
+          Настройки
+        </h1>
+      </div>
+
+      <div
+        className="
+          py-5
+          border-b
+          border-secondary-jet
+          border-solid
+        "
+      >
+        <Link
+          className="
+            flex
+            font-normal
+            text-sm
+            tracking-wide
+            text-on-primary-anti-flash-white
+            hover:text-on-primary-anti-flash-white
+            whitespace-nowrap
+            sm:justify-between
+          "
+          to="connected-apps"
+        >
+          <span>Подключенные приложения</span>
+          <img
+            className="
+              px-1.5
+              ml-2
+              rotate-180
+            "
+            src={arrowIcon}
+            alt="Arrow Icon"
+          />
+        </Link>
+      </div>
+
+      <div
+        className="
+          py-4
+          font-normal
+          text-sm
+          tracking-wide
+          sm:border-b
+          sm:border-secondary-jet
+          sm:border-solid
+        "
+      >
+        <button
+          className="
+            w-full
+            text-start
+            focus:outline-none
+          "
+          onClick={logout}
+        >
+          <span>Выход</span>
+        </button>
+      </div>
     </div>
   );
 });
