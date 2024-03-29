4032c2645be8fad86e92eccca2da5293bd634878
Andrey Starostin <andreystarostin.vio@gmail.com>
2024-03-30 00:27:17 +0300

    fix(TAS-318): TEST

--- src/components/TrackInfo/TrackInfo.tsx
+++ src/components/TrackInfo/TrackInfo.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
+import { Playback } from '@/modules/Player';
+
 type Props = {
   name: string;
   artist: string;
   provider: string;
+  url: string;
 };
 
 export const TrackInfo: React.FC<Props> = React.memo(
-  ({ name, artist, provider }) => {
+  ({ name, artist, provider, url }) => {
     return (
       <div className="flex">
         <div></div>
@@ -17,6 +20,11 @@ export const TrackInfo: React.FC<Props> = React.memo(
           <p>{provider}</p>
         </div>
         <div></div>
+        <Playback
+          tracks={[
+            'https://soundcloud.com/oembed?format=json&url=https://soundcloud.com/ozunapr/ozuna-david-guetta-vocation?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing',
+          ]}
+        ></Playback>
       </div>
     );
   },
