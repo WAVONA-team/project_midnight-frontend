19b24f35c76dfb02b4ab4108a96c7af712459090
Andrey Starostin <andreystarostin.vio@gmail.com>
2024-04-01 19:32:01 +0300

    fix(TAS-318): add track component without state

--- src/components/TrackInfo/TrackInfo.tsx
+++ src/components/TrackInfo/TrackInfo.tsx
@@ -1,30 +1,64 @@
-import React from 'react';
-
-import { Playback } from '@/modules/Player';
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
 
 type Props = {
   name: string;
   artist: string;
   provider: string;
+  imgUrl: string;
   url: string;
+  trackNumber: number;
+  isPlay: boolean;
+  currentTrack: number;
 };
 
+//состояние включенности если isPlay= true и trackNumber = trackNumberON
+
 export const TrackInfo: React.FC<Props> = React.memo(
-  ({ name, artist, provider, url }) => {
+  ({ name, artist, provider, imgUrl, url }) => {
+    const [duration, setDuration] = useState(0);
+
+    const getDuration = (state: number) => {
+      const min = state / 60;
+      const sec = state - 60 * Math.floor(min);
+      console.log('seconds)', sec);
+      setDuration(state);
+    };
+
     return (
-      <div className="flex">
-        <div></div>
-        <div>
-          <p>{name}</p>
-          <p>{artist}</p>
-          <p>{provider}</p>
+      <div className="flex p-2 px-4 bg-[black]">
+        <div
+          className="w-16 h-16 mr-2 rounded 
+        lg:mr-4
+        "
+        >
+          <img className="rounded" src={imgUrl} alt="Cover"></img>
         </div>
-        <div></div>
-        <Playback
-          tracks={[
-            'https://soundcloud.com/oembed?format=json&url=https://soundcloud.com/ozunapr/ozuna-david-guetta-vocation?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing',
-          ]}
-        ></Playback>
+
+        <div className="flex flex-col justify-between font-rubik lg:flex-row lg:items-center lg:flex-[4]">
+          <p className="text-on-primary-anti-flash-white text-base font-normal tracking-wide">
+            {name}
+          </p>
+          <p className="text-on-secondary-dim-gray text-sm font-normal tracking-[0.25px] ">
+            {artist}
+          </p>
+
+          <div className="flex items-center gap-1 text-on-secondary-dim-gray text-xs font-normal lg:flex-row-reverse lg:gap-2">
+            {provider}
+            <a className=" block w-[3.5px] h-[3.5px] rounded-full bg-on-secondary-dim-gray"></a>
+            {duration}
+          </div>
+        </div>
+        <div className="flex flex-1 justify-end items-center lg:justify-center  ">
+          <div className="flex gap-1 ">
+            <div className="w-[3px] h-[3px] rounded-sm bg-[transparent] border-[1px] border-on-primary-anti-flash-white border-solid "></div>
+            <div className="w-[3px] h-[3px] rounded-sm bg-[transparent] border-[1px] border-on-primary-anti-flash-white border-solid"></div>
+            <div className="w-[3px] h-[3px] rounded-sm bg-[transparent] border-[1px] border-on-primary-anti-flash-white border-solid"></div>
+          </div>
+        </div>
+        <div className="hidden">
+          <ReactPlayer url={url} playing={false} onDuration={getDuration} />
+        </div>
       </div>
     );
   },
--- src/pages/CreateNewTrackPage/CreateNewTrackPage.tsx
+++ src/pages/CreateNewTrackPage/CreateNewTrackPage.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 
 import { Controls, Playback } from '@/modules/Player';
 
 import { TrackInfo } from '@/components/TrackInfo/TrackInfo';
 
 export const CreateNewTrackPage: React.FC = React.memo(() => {
+  const handleDuration = (dur: number) => {
+    console.log('duration ', dur);
+  };
+
   return (
     <div className="w-full h-screen ">
-      <h1>CreateTrackPage</h1>
-      <Controls></Controls>
-      <Playback
-        tracks={[
-          'https://soundcloud.com/oembed?format=json&url=https://soundcloud.com/ozunapr/ozuna-david-guetta-vocation?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing',
-        ]}
-      ></Playback>
+      <TrackInfo
+        imgUrl="https://i1.sndcdn.com/artworks-ccQ0kQCf9M41-0-t500x500.jpg"
+        name="Boyz Don't Cry by Rod Wave"
+        artist="Rod Wave"
+        provider="SoundCloud"
+        url="https://soundcloud.com/oembed?format=json&url=https://soundcloud.com/rodwave/track-11?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+      />
     </div>
   );
 });
