b40eabd2f3a61d120f40f98136d35c6e5c8e6702
betterhell <dev.betterhell@gmail.com>
2024-04-05 11:55:56 +0300

    fix(TAS-342): Implement TrackShareButton

---
+++ src/ui/Button/TrackShareButton/TrackShareButton.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+import shareIcon from '@/assets/buttons/actionButtons/shareIcon.svg';
+
+type Props = {
+  trackName: string;
+  trackUrl: string;
+  className?: string;
+};
+
+const TIME_DELAY = 20000;
+
+const TrackShareButton: React.FC<Props> = React.memo(
+  ({ trackName, trackUrl, className = '' }) => {
+    const [isClicked, setIsClicked] = useState<boolean>(false);
+    let timeId: NodeJS.Timeout | null;
+
+    const copyToClipboard = async () => {
+      if (!navigator.share) {
+        await navigator.clipboard.writeText(trackUrl);
+        if (timeId) {
+          clearTimeout(timeId);
+        }
+        setIsClicked(true);
+        timeId = setTimeout(() => {
+          setIsClicked(false);
+        }, TIME_DELAY);
+      } else {
+        const shareData = {
+          title: trackName,
+          url: trackUrl,
+        };
+        await navigator.share(shareData);
+      }
+    };
+
+    return (
+      <button
+        onClick={copyToClipboard}
+        className={`
+          ${className}
+          bg-[inherit]
+          w-full
+          border-t-0
+          border-l-0
+          border-r-0
+          border-b
+          border-secondary-eerie-black-light
+          border-solid
+          outline-none
+          focus:outline-none
+          hover:border-secondary-eerie-black-light
+        `}
+      >
+        <div className="flex p-4">
+          <img src={shareIcon} alt="Share" />
+          <span
+            className={`
+              ${!isClicked ? 'opacity-100' : 'opacity-5'}
+              text-on-primary-anti-flash-white
+              ml-4
+              font-normal
+              text-base
+              tracking-widest
+            `}
+          >
+            {isClicked ? 'Скопировано!' : 'Поделиться'}
+          </span>
+        </div>
+      </button>
+    );
+  },
+);
+
+export default TrackShareButton;
