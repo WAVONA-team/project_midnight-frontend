feabd092e008d7dacfc9448cf308c576a0d8af29
Andrey Starostin <andreystarostin.vio@gmail.com>
2024-04-08 18:30:44 +0300

    fix(TAS-318): clean CreateNewTrackPage

--- src/pages/CreateNewTrackPage/CreateNewTrackPage.tsx
+++ src/pages/CreateNewTrackPage/CreateNewTrackPage.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-import { TrackInfo } from '@/components/TrackInfo/TrackInfo';
-
 export const CreateNewTrackPage: React.FC = React.memo(() => {
-  return (
-    <>
-      <TrackInfo />
-    </>
-  );
+  return <></>;
 });
