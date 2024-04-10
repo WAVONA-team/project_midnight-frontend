e454c09e7fbc581b902edb4dce273b0e9818ea6d
aguilam <pexfex@mail.ru>
2024-04-10 21:05:22 +0400

    fix(tas-283): added map for links and classNames

--- src/modules/NavBar/index.tsx
+++ src/modules/NavBar/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import classNames from 'classnames';
+
 const NavBar: React.FC = React.memo(() => {
+  const linksInfo = [
+    {
+      img: 'src/assets/navBar/home.svg',
+      title: 'Главная',
+      link: '/tracks',
+    },
+    {
+      img: 'src/assets/navBar/playlist.svg',
+      title: 'Плейлисты',
+      link: '/playlists',
+    },
+    {
+      img: 'src/assets/navBar/addTrack.svg',
+      title: 'Добавить трек',
+      link: '/tracks/new',
+    },
+  ];
   return (
     <aside className="w-full h-16 py-3 flex-row  bg-surface-eerie_black flex justify-between lg:w-72 lg:h-full lg:py-2 lg:flex-col font-rubik">
       <div className=" w-full ">
@@ -18,54 +37,24 @@ const NavBar: React.FC = React.memo(() => {
           </div>
         </div>
         <div className="grid grid-cols-3 px-3 justify-items-center w-full lg:flex lg:flex-col lg:px-0">
-          <NavLink
-            className={({ isActive }) =>
-              isActive
-                ? 'bg-secondary-eerie-black-light text-on-primary-anti-flash-white hover:text-on-primary-lavender-blush flex flex-col items-center pl-0 py-0 gap-0 lg:flex-row lg:pl-6 lg:py-4 lg:gap-5'
-                : 'text-secondary-cadet-gray flex flex-col items-center pl-0 py-0 gap-0 lg:flex-row lg:pl-6 lg:py-4 lg:gap-5 hover:bg-secondary-jet hover:text-on-primary-lavender-blush'
-            }
-            to="/tracks" end
-          >
-            <img
-              src="src\assets\navBar\home.svg"
-              alt=""
-              height={18}
-              width={18}
-            />
-            <p>Главная</p>
-          </NavLink>
-          <NavLink
-            className={({ isActive }) =>
-              isActive
-                ? 'bg-secondary-eerie-black-light text-on-primary-anti-flash-white hover:text-on-primary-lavender-blush flex flex-col items-center pl-0 py-0 gap-0 lg:flex-row lg:pl-6 lg:py-4 lg:gap-5'
-                : 'text-secondary-cadet-gray flex flex-col items-center pl-0 py-0 gap-0 lg:flex-row lg:pl-6 lg:py-4 lg:gap-5 hover:bg-secondary-jet hover:text-on-primary-lavender-blush'
-            }
-            to="/playlists"
-          >
-            <img
-              src="src\assets\navBar\playlist.svg"
-              alt=""
-              height={20}
-              width={20}
-            />
-            <p>Плейлисты</p>
-          </NavLink>
-          <NavLink
-            className={({ isActive }) =>
-              isActive
-                ? 'bg-secondary-eerie-black-light text-on-primary-anti-flash-white hover:text-on-primary-lavender-blush flex flex-col items-center pl-0 py-0 gap-0 lg:flex-row lg:pl-6 lg:py-4 lg:gap-5'
-                : 'text-secondary-cadet-gray flex flex-col items-center pl-0 py-0 gap-0 lg:flex-row lg:pl-6 lg:py-4 lg:gap-5 hover:bg-secondary-jet hover:text-on-primary-lavender-blush'
-            }
-            to="/tracks/new"
-          >
-            <img
-              src="src\assets\navBar\addTrack.svg"
-              alt=""
-              height={20}
-              width={20}
-            />
-            <p>Добавить трек</p>
-          </NavLink>
+          {linksInfo.map((link, index) => (
+            <NavLink
+              className={({ isActive }) =>
+                classNames({
+                  'text-on-primary-anti-flash-white bg-secondary-eerie-black-light hover:text-on-primary-lavender-blush flex flex-col items-center pl-0 py-0 gap-0 lg:flex-row lg:pl-6 lg:py-4 lg:gap-5':
+                    isActive,
+                  'text-secondary-cadet-gray hover:bg-secondary-jet flex flex-col items-center pl-0 py-0 gap-0 lg:flex-row lg:pl-6 lg:py-4 lg:gap-5 hover:text-on-primary-lavender-blush':
+                    !isActive,
+                })
+              }
+              to={link.link}
+              end
+              key={index}
+            >
+              <img src={link.img} alt="" height={18} width={18} />
+              <p>{link.title}</p>
+            </NavLink>
+          ))}
         </div>
       </div>
       <NavLink
