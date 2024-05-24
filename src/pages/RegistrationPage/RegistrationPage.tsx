12bf32c6b20117c3f0311c98bb239deaff63549d
betterhell <dev.betterhell@gmail.com>
2024-05-24 20:53:57 +0300

    fix(TEST-5): Fix reg/auth pages styles

--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -14,13 +14,21 @@ export const RegistrationPage: React.FC = React.memo(() => {
         bg-background-hight
         sm:h-full
         lg:bg-[url('/home_bg_desktop.webp')]
+        md:h-screen
+        landscape:sm:h-fit
+        sm:h-full
         lg:bg-no-repeat
         lg:bg-center
         lg:bg-cover
       "
     >
+<<<<<<< HEAD
       <main className="lg:grid lg:grid-cols-2 items-center">
         <Container>
+=======
+      <Container className="flex flex-col justify-center lg:h-screen">
+        <main className="lg:grid lg:grid-cols-2 items-center">
+>>>>>>> fcb6100 (fix(TEST-5): Fix reg/auth pages styles)
           <Logo className="hidden lg:flex" />
         </Container>
 
