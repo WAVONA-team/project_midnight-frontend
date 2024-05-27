6944523352227c8c21210c7c850d791c42a33c11
aguilam <pexfex@mail.ru>
2024-05-27 20:50:50 +0400

    fix(test-2): fixed playbutton position

--- src/modules/Player/components/Controls/Controls.tsx
+++ src/modules/Player/components/Controls/Controls.tsx
@@ -41,7 +41,7 @@ export const Controls: React.FC = React.memo(() => {
       <div className="grid max-sm:grid-cols-4 md:grid-cols-[1fr_min-content] lg:grid-cols-3 justify-items-center items-center mt-3 px-4 lg:px-0">
         <TrackInformation />
 
-        <div className="block lg:hidden">
+        <div className="lg:hidden flex justify-end w-full">
           <PlayButton />
         </div>
 
