b11e47999be8a11e5a1fafac87008ea8075edaf4
Vo7kov <jegor.volkov.a@gmail.com>
2024-05-28 21:20:33 +0200

    fix(DEV-3): fix gif name

--- src/ui/Spinner/Spinner.tsx
+++ src/ui/Spinner/Spinner.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import SpinnerImg from '@/../public/loader/loader ж®™Ђ.gif';
+import SpinnerImg from '@/../public/loader/loader.gif';
 
 type Props = {
   width?: string;
