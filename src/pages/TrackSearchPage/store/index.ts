fc40ffd7f7f3bd2c8b35eb8ce2b84bc42a9ee2fc
Vo7kov <jegor.volkov.a@gmail.com>
2024-05-30 19:55:56 +0200

    fix(DEV-15): remove comments

--- src/pages/TrackSearchPage/store/index.ts
+++ src/pages/TrackSearchPage/store/index.ts
@@ -1,8 +1,5 @@
-// import { Track } from 'project_midnight';
 import { StateCreator } from 'zustand';
 
-// import { httpClient } from '@/shared/api/httpClient';
-// import { ServerErrors } from '@/shared/types/ServerErrors';
 import { TracksSearchPageState } from './types';
 
 export const tracksSearchPageSlice: StateCreator<TracksSearchPageState> = (
