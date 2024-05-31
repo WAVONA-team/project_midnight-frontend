c351736e479ab75ac4eb09c291f074c1ceaea6b9
betterhell <dev.betterhell@gmail.com>
2024-05-31 15:59:19 +0300

    fix(TEST-20): Fix fading spinner, add some animation

--- src/modules/TrackHistory/components/TrackHistory.tsx
+++ src/modules/TrackHistory/components/TrackHistory.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { useStore } from '@/store';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import { TrackInfo } from '@/components/TrackInfo';
 
@@ -41,26 +42,35 @@ export const TrackHistory: React.FC = React.memo(() => {
       </Container>
 
       <div className="flex flex-col gap-3 mt-3">
-        {userSearchHistory?.map((track) => (
-          <TrackInfo
-            key={track.id}
-            name={track.title}
-            artist={track.author as string}
-            provider={track.source}
-            duration={track.duration}
-            imgUrl={track.imgUrl as string}
-            isPlay={currentTrack?.url === track.url}
-            handlerPlay={() => {
-              changeCurrentTrack(track);
-              changePlayerState(
-                currentTrack?.url !== track.url ? true : !playerState,
-              );
+        <AnimatePresence>
+          {userSearchHistory?.map((track) => (
+            <motion.div
+              key={track.id}
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              transition={{ duration: 0.2 }}
+            >
+              <TrackInfo
+                name={track.title}
+                artist={track.author as string}
+                provider={track.source}
+                duration={track.duration}
+                imgUrl={track.imgUrl as string}
+                isPlay={currentTrack?.url === track.url}
+                handlerPlay={() => {
+                  changeCurrentTrack(track);
+                  changePlayerState(
+                    currentTrack?.url !== track.url ? true : !playerState,
+                  );
 
-              updateHistoryOrder(track.id);
-            }}
-            handlerModal={() => {}}
-          />
-        ))}
+                  updateHistoryOrder(track.id);
+                }}
+                handlerModal={() => {}}
+              />
+            </motion.div>
+          ))}
+        </AnimatePresence>
       </div>
     </div>
   );
