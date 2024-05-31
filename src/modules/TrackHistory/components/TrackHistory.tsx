902941d4096dfc59a6cc0937396625006fcabaf8
betterhell <dev.betterhell@gmail.com>
2024-05-31 15:59:19 +0300

    fix(TEST-20): Fix fading spinner, add some animation

--- src/modules/TrackHistory/components/TrackHistory.tsx
+++ src/modules/TrackHistory/components/TrackHistory.tsx
@@ -49,7 +49,7 @@ export const TrackHistory: React.FC = React.memo(() => {
               initial={{ opacity: 0, y: -10 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: -10 }}
-              transition={{ duration: 0.3 }}
+              transition={{ duration: 0.2 }}
             >
               <TrackInfo
                 name={track.title}
