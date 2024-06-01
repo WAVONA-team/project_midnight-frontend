89bef5f15c286c27df22ba1899a88e1c3209185f
Vo7kov <jegor.volkov.a@gmail.com>
2024-06-01 15:02:41 +0200

    fix(TEST-5): fix virtual keyboard markup

--- src/pages/ConfirmationResetPasswordPage/ConfirmationResetPasswordPage.tsx
+++ src/pages/ConfirmationResetPasswordPage/ConfirmationResetPasswordPage.tsx
@@ -9,21 +9,23 @@ export const ConfirmationResetPasswordPage: React.FC = React.memo(() => {
   return (
     <div
       className="
-    app
-    h-screen
-    bg-background-hight
-    lg:bg-[url('/home_bg_desktop.webp')]
-    lg:bg-no-repeat
-    lg:bg-center
-    lg:bg-cover
-  "
+        app
+        h-dvh
+        bg-background-hight
+        sm:h-full
+        lg:bg-[url('/home_bg_desktop.webp')]
+        lg:bg-no-repeat
+        lg:bg-center
+        lg:bg-cover
+      "
     >
-      <Container className="h-full flex flex-col justify-center">
-        <main className="lg:grid lg:grid-cols-2 items-center">
+      <main className="lg:grid lg:grid-cols-2 items-center">
+        <Container>
           <Logo className="hidden lg:flex" />
-          <ConfirmationResetPasswordForm />
-        </main>
-      </Container>
+        </Container>
+
+        <ConfirmationResetPasswordForm />
+      </main>
     </div>
   );
 });
