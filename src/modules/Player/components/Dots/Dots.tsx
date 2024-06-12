b20934179c5e975f4ba6b4741f18a754f2030394
Vo7kov <jegor.volkov.a@gmail.com>
2024-06-12 20:13:23 +0200

    fix(MASTER): fix modal in playback element

--- src/modules/Player/components/Dots/Dots.tsx
+++ src/modules/Player/components/Dots/Dots.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import { useStore } from '@/store';
 import { Menu } from '@headlessui/react';
 
 import Dropdown from '@/components/Dropdown/Dropdown.tsx';
+import Portal from '@/components/Portal/Portal';
 
 import { TrackShareButton } from '@/ui/Button';
 import AddToPlaylist from '@/ui/Button/MenuButton/AddToPlaylistButton/AddToPlaylistButton';
@@ -16,39 +17,77 @@ export const Dots: React.FC = React.memo(() => {
   const { currentTrack } = useStore(({ currentTrack }) => ({
     currentTrack,
   }));
+
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const openMenu = () => {
-    setIsOpen(!isOpen);
+  const [childElement, setChildElement] = useState<HTMLElement | null>(null);
+  const ref = useRef(null);
+
+  const handlerModal = ({
+    currentTarget,
+  }: React.MouseEvent<HTMLDivElement> & { trackId?: string }) => {
+    if (currentTarget === childElement) {
+      setIsOpen((state) => !state);
+    } else {
+      const element = currentTarget as HTMLElement;
+
+      setChildElement(element);
+
+      setIsOpen((state) => !state);
+    }
   };
+
   return (
     <div>
-      <button type="button" onClick={openMenu} className="focus:outline-none">
-        <DotsIcon active={isOpen} />
-      </button>
+      <Menu as="div">
+        <div
+          onClick={handlerModal}
+          className="relative"
+          ref={ref}
+          onBlur={() => setIsOpen(false)}
+          tabIndex={0}
+        >
+          <button type="button" className="focus:outline-none">
+            <DotsIcon active={isOpen} />
+          </button>
+        </div>
 
-      <Menu as="div" className="relative ">
-        <Dropdown className="" modalOnCloseHandler={() => {}}>
-          <div
+        <Portal openPortal={isOpen} element={childElement}>
+          <Dropdown
             className="
-              bg-surface-eerie_black
-              w-[370px]
-              right-5
-              bottom-20
-              absolute
-              shadow-dropdown-bottom-shadow
-              z-10
+              sm:right-0
+              sm:top-8
+              sm:w-[254px]
+              sm:absolute
+              py-4
+              sm:py-0
+              shadow-[16px_-16px_16px_0px_#0C0D0B80]
             "
+            modalOnCloseHandler={() => setIsOpen(false)}
           >
-            <TrackSaveOnMainButton className="" />
-            <AddToPlaylist className="rounded-none" />
-            <AddToQueueButton className="rounded-none" />
-            <TrackShareButton
-              selectedTrack={currentTrack!}
-              className="rounded-none"
-            />
-            <DeleteTrackButton className="" />
-          </div>
-        </Dropdown>
+            <Menu.Item as="div">
+              <TrackSaveOnMainButton className="" />
+            </Menu.Item>
+
+            <Menu.Item as="div">
+              <AddToPlaylist className="rounded-none" />
+            </Menu.Item>
+
+            <Menu.Item as="div">
+              <AddToQueueButton className="rounded-none" />
+            </Menu.Item>
+
+            <Menu.Item as="div">
+              <TrackShareButton
+                selectedTrack={currentTrack!}
+                className="rounded-none"
+              />
+            </Menu.Item>
+
+            <Menu.Item as="div">
+              <DeleteTrackButton className="" />
+            </Menu.Item>
+          </Dropdown>
+        </Portal>
       </Menu>
     </div>
   );
