dc87c01cfbc1b31dceaeb7255a1a41a803a0916b
aguilam <pexfex@mail.ru>
2024-06-18 19:34:24 +0400

    feat(dev-22): added filtering for music

--- src/pages/TrackSearchPage/store/index.ts
+++ src/pages/TrackSearchPage/store/index.ts
@@ -1,4 +1,6 @@
+
 import { StateCreator } from 'zustand';
+import { Track } from 'project_midnight';
 
 import { TracksSearchPageState } from './types';
 
@@ -6,7 +8,13 @@ export const tracksSearchPageSlice: StateCreator<TracksSearchPageState> = (
   set,
 ) => ({
   query: '',
-  setQuery: (query) => set({ query }),
+  sortType: 'updatedAt',
+  order: 'desc',
+  isFiltering: false,
+  setQuery: (query: string) => set({ query }),
+  setSortType: (sortType: keyof Track) => set({ sortType }),
+  setOrder: (order: 'desc' | 'asc' ) => set({ order }),
+  setIsFiltering: (isFiltering: boolean) => set({ isFiltering }),
 });
 
 export { type TracksSearchPageState };
