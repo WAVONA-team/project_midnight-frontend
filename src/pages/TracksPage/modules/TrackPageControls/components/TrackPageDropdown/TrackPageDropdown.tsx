92b21db1dbce46885eda888d97c186b13fcf99e7
aguilam <pexfex@mail.ru>
2024-06-18 19:34:24 +0400

    feat(dev-22): added filtering for music

--- src/pages/TracksPage/modules/TrackPageControls/components/TrackPageDropdown/TrackPageDropdown.tsx
+++ src/pages/TracksPage/modules/TrackPageControls/components/TrackPageDropdown/TrackPageDropdown.tsx
@@ -1,7 +1,9 @@
 import React, { useLayoutEffect, useRef, useState } from 'react';
 
 import { useStore } from '@/store';
+import { useStore } from '@/store';
 import { Menu } from '@headlessui/react';
+import { Track } from 'project_midnight';
 import { Track } from 'project_midnight';
 
 import { tracksSearchPageSlice } from '@/pages/TrackSearchPage/store';
