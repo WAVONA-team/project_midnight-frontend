d43fd73b6c9ff7df673951e5cfcf90087945865f
Vo7kov <jegor.volkov.a@gmail.com>
2024-06-22 17:44:40 +0200

    fix(TEST-30): fix soundcloud and spotify playing

--- src/modules/TrackAddition/store/types/TrackAdditionState.ts
+++ src/modules/TrackAddition/store/types/TrackAdditionState.ts
@@ -2,11 +2,14 @@ import { Track } from 'project_midnight';
 
 export type TrackAdditionState = {
   parsedTrack: Track | null;
-  setParsedTrack: (track: Track) => void,
+  setParsedTrack: (track: Track) => void;
   parseTrack: (url: string, userId: string, duration: string) => Promise<Track>;
   parsedTrackDuration: string | null;
   setParsedTrackDuration: (state: string | null) => void;
   isParsedTrackLoading: boolean;
   setIsParsedTrackLoading: (state: boolean) => void;
   clearParsedTrack: () => void;
+  resolvedUrl: string | null;
+  setResolvedUrl: (url: string | null) => void;
+  resolveShortUrl: (url: string) => Promise<void>;
 };
