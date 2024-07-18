177b2c0cfb0805f564e24846dde0c853ae01250b
Vo7kov <jegor.volkov.a@gmail.com>
2024-07-18 17:21:31 +0200

    refactor(DEV-33): 1 more small refactor

--- src/pages/TracksPage/modules/TrackPageControls/store/types/TracksPlaylistControlsState.ts
+++ src/pages/TracksPage/modules/TrackPageControls/store/types/TracksPlaylistControlsState.ts
@@ -1,6 +1,6 @@
 export type TracksPlaylistControlsState = {
   tracksTitle: 'Пауза' | 'Слушать';
   tracksIcon: string;
-  setTracksTitle: (title: string) => void;
+  setTracksTitle: (title: 'Пауза' | 'Слушать') => void;
   setTracksIcon: (icon: string) => void;
 };
