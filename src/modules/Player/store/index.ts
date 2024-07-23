40525256829b686cff05f43278105b4718459302
betterhell <dev.betterhell@gmail.com>
2024-07-23 12:46:06 +0300

    feat(DEV-34): Zustand persist added for player states

--- src/modules/Player/store/index.ts
+++ src/modules/Player/store/index.ts
@@ -1,27 +1,41 @@
 import { Track } from 'project_midnight';
 import { type StateCreator } from 'zustand';
+import { PersistOptions, createJSONStorage, persist } from 'zustand/middleware';
 
 import { PlayerState } from './types/PlayerState';
 
-export const createPlayerSlice: StateCreator<PlayerState> = (set) => ({
-  playerState: false,
-  isLoop: false,
-  volume: 0.7,
-  currentTrack: null,
-  currentTime: 0,
-  secondsLoaded: 0,
-  duration: 0,
-  seekTo: 0,
-  seeking: false,
-  changePlayerState: (playerState: boolean) => set({ playerState }),
-  changeIsLoop: (isLoop: boolean) => set({ isLoop }),
-  changeVolume: (volume: number) => set({ volume }),
-  changeCurrentTrack: (currentTrack: Track | null) => set({ currentTrack }),
-  changeCurrentTime: (currentTime: number) => set({ currentTime }),
-  changeSecondsLoaded: (secondsLoaded: number) => set({ secondsLoaded }),
-  changeDuration: (duration: number) => set({ duration }),
-  changeSeekTo: (seekTo: number) => set({ seekTo }),
-  changeSeeking: (seeking: boolean) => set({ seeking }),
-});
+type MyPersist = (
+  config: StateCreator<PlayerState>,
+  options: PersistOptions<PlayerState>,
+) => StateCreator<PlayerState>;
+
+export const createPlayerSlice: StateCreator<PlayerState, []> = (
+  persist as MyPersist
+)(
+  (set) => ({
+    playerState: false,
+    isLoop: false,
+    volume: 0.7,
+    currentTrack: null,
+    currentTime: 0,
+    secondsLoaded: 0,
+    duration: 0,
+    seekTo: 0,
+    seeking: false,
+    changePlayerState: (playerState: boolean) => set({ playerState }),
+    changeIsLoop: (isLoop: boolean) => set({ isLoop }),
+    changeVolume: (volume: number) => set({ volume }),
+    changeCurrentTrack: (currentTrack: Track | null) => set({ currentTrack }),
+    changeCurrentTime: (currentTime: number) => set({ currentTime }),
+    changeSecondsLoaded: (secondsLoaded: number) => set({ secondsLoaded }),
+    changeDuration: (duration: number) => set({ duration }),
+    changeSeekTo: (seekTo: number) => set({ seekTo }),
+    changeSeeking: (seeking: boolean) => set({ seeking }),
+  }),
+  {
+    name: 'playerStorage',
+    storage: createJSONStorage(() => localStorage),
+  },
+);
 
 export { type PlayerState };
